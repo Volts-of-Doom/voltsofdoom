2d36387c1f1319c16aea4b83d2a5a2fb79310ec9
GenElectrovise <sanchez.pamelos@outlook.com>
2021-04-03 20:15:17 +0100

    Reworking reflectories

--- zapbyte/src/main/java/vision/voltsofdoom/zapbyte/reflectory/Reflectories.java
+++ zapbyte/src/main/java/vision/voltsofdoom/zapbyte/reflectory/Reflectories.java
@@ -6,10 +6,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Set;
-import org.reflections.scanners.MethodAnnotationsScanner;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.scanners.TypeAnnotationsScanner;
-import org.reflections.util.FilterBuilder;
 import vision.voltsofdoom.zapbyte.loader.VODClassLoader;
 import vision.voltsofdoom.zapbyte.main.ZapByte;
 import vision.voltsofdoom.zapbyte.resource.JarMapper;
@@ -19,67 +15,11 @@ public class Reflectories {
    * A {@link LinkedHashMap} of {@link Reflectory}s -- one for each jar file found by
    * {@link JarMapper#find()}, with their keys being the names of the files which they are bound to.
    */
-  private static LinkedHashMap<String, Reflectory> reflectories =
-      new LinkedHashMap<String, Reflectory>();
+  private static LinkedHashMap<String, Reflectory> reflectories = new LinkedHashMap<String, Reflectory>();
 
-  private static HashMap<String, Class<?>> additionalClassesToGenerateReflectories =
-      new HashMap<String, Class<?>>();
+  private static HashMap<String, Class<?>> additionalClassesToGenerateReflectories = new HashMap<String, Class<?>>();
 
-  /**
-   * @return A {@link Reflectory.Builder} with default properties. Does not contain a
-   *         {@link ClassLoader}. <br>
-   *         Excludes packages:
-   *         <ul>
-   *         <li>java
-   *         <li>com.sun
-   *         <li>org.reflections
-   *         <li>com.google
-   *         </ul>
-   */
-  public static Reflectory.Builder defaultBuilder() {
-    Reflectory.Builder builder = new Reflectory.Builder();
-    builder.withScanner(new TypeAnnotationsScanner());
-    builder.withScanner(new MethodAnnotationsScanner());
-    builder.withScanner(new SubTypesScanner(false));
-    builder.withFilterBuilder(defaultFilterBuilder());
-    return builder;
-  }
 
-  /**
-   * Excludes a ton of stuff. Trust me you don't want this stuff. If you do want this stuff, but
-   * it's excluded here, just use this, and use {@link FilterBuilder#include(String)} or one of the
-   * companion methods to get it back. This way is better trust me... (cuts logs from 2.5MB to 0.1MB,
-   * as of 17/11/20 by excluding jars which give "failed to scan" logs)
-   */
-  public static FilterBuilder defaultFilterBuilder() {
-    return new FilterBuilder()//
-        // Exclude sources
-        .excludePackage("java")//
-        .excludePackage("lib/").exclude("lib/")//
-        .excludePackage("lib.").exclude("lib.")//
-        .excludePackage("resources/").exclude("resources/")//
-        .excludePackage("resources.").exclude("resources.")//
-        .excludePackage("META-INF/").exclude("META-INF/")//
-        .excludePackage("META-INF.").exclude("META-INF.")//
-        .excludePackage("org.reflections").excludePackage("org/reflections")//
-        .excludePackage("com.google").excludePackage("com/google")//
-        .excludePackage("org.lwjgl").excludePackage("org/lwjgl")//
-        .excludePackage("ch.qos").excludePackage("ch/qos")//
-        .excludePackage("edu.umd").excludePackage("edu/umd")//
-        .excludePackage("jdk.nashorn").excludePackage("jdk/nashorn")//
-        .excludePackage("jdk.internal").excludePackage("jdk/internal")//
-        .excludePackage("windows.x64").excludePackage("windows/x64")//
-        .excludePackage("net.jcip").excludePackage("net/jcip")//
-        .excludePackage("com.sun").excludePackage("com/sun")//
-        .excludePackage("sun.text").excludePackage("sun/text")//
-
-        // Exclude resources
-        .excludePackage("image/").exclude("image/")//
-        .excludePackage("image.").exclude("image.")//
-        .excludePackage("font/").exclude("font/")//
-        .excludePackage("font.").exclude("font.")//
-    ;
-  }
 
   /**
    * Searches the {@link #reflectories} list, and returns the {@link Reflectory} bound to the given
@@ -108,7 +48,10 @@ public class Reflectories {
 
       try {
 
-        Reflectory.Builder builder = defaultBuilder();
+        ReflectoryBuilder builder = new ReflectoryBuilder();
+        builder.withScanners(ReflectoryBuilder.getDefaultScanners());
+        builder.withFilterBuilder(ReflectoryBuilder.getDefaultFilterBuilder());
+
         URL[] urls = new URL[] {file.toURI().toURL()};
         builder.withClassLoader(new VODClassLoader(urls));
         builder.withVisibleName(file.getName());
@@ -121,9 +64,13 @@ public class Reflectories {
 
     }
 
+    // TODO FIND ANOTHER WAY TO ADD ADDITIONALS
+
     // Additional
     for (String key : additionalClassesToGenerateReflectories.keySet()) {
-      Reflectory.Builder builder = defaultBuilder();
+      ReflectoryBuilder builder = new ReflectoryBuilder();
+      builder.withScanners(ReflectoryBuilder.getDefaultScanners());
+      builder.withFilterBuilder(ReflectoryBuilder.getDefaultFilterBuilder());
 
       ClassLoader loader = additionalClassesToGenerateReflectories.get(key).getClassLoader();
       builder.withVisibleName(key);
@@ -133,8 +80,12 @@ public class Reflectories {
       reflectories.putIfAbsent(key, ref);
     }
 
+    // TODO DO ELSEWHERE!!
+
     // ZapByte
-    Reflectory.Builder builder = defaultBuilder();
+    ReflectoryBuilder builder = new ReflectoryBuilder();
+    builder.withScanners(ReflectoryBuilder.getDefaultScanners());
+    builder.withFilterBuilder(ReflectoryBuilder.getDefaultFilterBuilder());
 
     ClassLoader loader = ZapByte.class.getClassLoader();
     builder.withVisibleName("zapbyte");
--- zapbyte/src/main/java/vision/voltsofdoom/zapbyte/reflectory/Reflectory.java
+++ zapbyte/src/main/java/vision/voltsofdoom/zapbyte/reflectory/Reflectory.java
@@ -1,6 +1,5 @@
 package vision.voltsofdoom.zapbyte.reflectory;
 
-import java.util.ArrayList;
 import org.reflections.Reflections;
 import org.reflections.scanners.Scanner;
 import org.reflections.util.ClasspathHelper;
@@ -15,18 +14,17 @@ import org.reflections.util.FilterBuilder;
  */
 public class Reflectory {
 
-  private ClassLoader classLoader;
+  private ClassLoader[] classLoaders;
   private Scanner[] scanners;
   private Reflections reflections;
   private String visibleName;
   private FilterBuilder filterBuilder;
+  private String[] packages;
 
   /**
-   * Prevents usage of the blank constructor. <b>Use {@link Reflectory.Builder}</b>
+   * <b>Use {@link ReflectoryBuilder}</b>
    */
-  private Reflectory() {
-
-  }
+  protected Reflectory() {}
 
   /**
    * Creates this {@link Reflectory}'s instance of {@link Reflections}, which indexes the classes
@@ -36,17 +34,18 @@ public class Reflectory {
    */
   public Reflectory index() {
     ConfigurationBuilder configBuilder = new ConfigurationBuilder();
-    configBuilder.setUrls(ClasspathHelper.forClassLoader(classLoader));
-    configBuilder.addClassLoader(classLoader);
+    configBuilder.setUrls(ClasspathHelper.forClassLoader(classLoaders));
+    configBuilder.addClassLoaders(classLoaders);
     configBuilder.addScanners(scanners);
     configBuilder.setInputsFilter(filterBuilder);
+    configBuilder.forPackages(packages);
 
     setReflections(new Reflections(configBuilder));
     return this;
   }
 
-  public ClassLoader getClassLoader() {
-    return classLoader;
+  public ClassLoader[] getClassLoaders() {
+    return classLoaders;
   }
 
   public Scanner[] getScanners() {
@@ -75,21 +74,21 @@ public class Reflectory {
   /**
    * @param filterBuilder the filterBuilder to set
    */
-  private void setFilterBuilder(FilterBuilder filterBuilder) {
+  void setFilterBuilder(FilterBuilder filterBuilder) {
     this.filterBuilder = filterBuilder;
   }
 
   /**
    * @param classLoader the classLoader to set
    */
-  private void setClassLoader(ClassLoader classLoader) {
-    this.classLoader = classLoader;
+  void setClassLoaders(ClassLoader[] classLoaders) {
+    this.classLoaders = classLoaders;
   }
 
   /**
    * @param scanners the scanners to set
    */
-  private void setScanners(Scanner[] scanners) {
+  void setScanners(Scanner[] scanners) {
     this.scanners = scanners;
   }
 
@@ -98,62 +97,5 @@ public class Reflectory {
    */
   private void setReflections(Reflections reflections) {
     this.reflections = reflections;
-  }
-
-  /**
-   * Constructs a {@link Reflectory} using chained methods. Retrieve the completed
-   * {@link Reflectory} using {@link #build()}
-   * 
-   * @author GenElectrovise
-   *
-   */
-  public static class Builder {
-
-    private Reflectory reflectory;
-    private ArrayList<Scanner> scanners = new ArrayList<Scanner>();
-
-    public Builder() {
-      this.reflectory = new Reflectory();
-    }
-
-    /**
-     * Gives the provided {@link ClassLoader} to the constructing {@link Reflectory}, which will be
-     * used to index the correct tree of jars.
-     * 
-     * @param classLoader
-     * @return This {@link Builder}
-     */
-    public Builder withClassLoader(ClassLoader classLoader) {
-      reflectory.setClassLoader(classLoader);
-      return this;
-    }
-
-    /**
-     * Applies a {@link Scanner} to the constructing {@link Reflectory}, which will be used to
-     * retrieve attributes from an indexed jar.
-     * 
-     * @param scanner
-     * @return This {@link Builder}
-     */
-    public Builder withScanner(Scanner scanner) {
-      this.scanners.add(scanner);
-      return this;
-    }
-
-    public Reflectory build() {
-      reflectory.setScanners(this.scanners.toArray(new Scanner[scanners.size()]));
-      reflectory.index();
-      return reflectory;
-    }
-
-    public Builder withVisibleName(String name) {
-      reflectory.setVisibleName(name);
-      return this;
-    }
-
-    public Builder withFilterBuilder(FilterBuilder filterBuilder) {
-      reflectory.setFilterBuilder(filterBuilder);
-      return this;
-    }
   }
 }
