1bf06afebd3048948948bab12525edb99da861ac
Richard Spencer <richard_n_spencer@hotmail.com>
2021-04-07 16:34:39 +0100

    Created test runner for easier viewing of Silverspark

---
+++ silverspark/src/main/java/SilversparkTestRunner.java
@@ -0,0 +1,22 @@
+import vision.voltsofdoom.silverspark.core.Game;
+import vision.voltsofdoom.silverspark.core.VariableTimestepGame;
+
+public class SilversparkTestRunner implements Runnable {
+
+	public SilversparkTestRunner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		SilversparkTestRunner runner = new SilversparkTestRunner();
+		runner.run();
+	}
+	
+	@Override
+	public void run() {
+	    System.out.println("Running Silverspark manual test application");
+	    Game game = new VariableTimestepGame("Volts of Doom");
+	    game.start();
+	}
+
+}
