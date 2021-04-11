cd6410f9630e1ac394a92c17991125c47f064aa4
GenElectrovise <sanchez.pamelos@outlook.com>
2021-04-11 16:26:43 +0100

    Added Logger for TypeRegistries class

--- voltsofdoom/src/main/java/vision/voltsofdoom/voltsofdoom/universal/registry/TypeRegistries.java
+++ voltsofdoom/src/main/java/vision/voltsofdoom/voltsofdoom/universal/registry/TypeRegistries.java
@@ -1,5 +1,7 @@
 package vision.voltsofdoom.voltsofdoom.universal.registry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import vision.voltsofdoom.voltsofdoom.play.adventure.Adventure;
 import vision.voltsofdoom.voltsofdoom.play.entity.Entity;
 import vision.voltsofdoom.voltsofdoom.play.tile.Tile;
@@ -10,22 +12,20 @@ import vision.voltsofdoom.zapbyte.registry.TypeRegistry;
 import vision.voltsofdoom.zapbyte.resource.ResourceLocation;
 
 public class TypeRegistries {
+  
+  public static final Logger LOGGER = LoggerFactory.getLogger(TypeRegistries.class);
 
   public static TypeRegistry<Tile> TILES;
   public static TypeRegistry<Entity> ENTITIES;
   public static TypeRegistry<Adventure> ADVENTURES;
 
   @Stowaway
-  private static void createAndSubmitTypeRegistries(
-      RegistryEvent.CreateAndSubmitRegistriesEvent event) {
-    VoltsOfDoom.easyDebug("Creating and submitting default TypeRegistries");
+  private static void createAndSubmitTypeRegistries(RegistryEvent.CreateAndSubmitRegistriesEvent event) {
+    LOGGER.debug("Creating and submitting default TypeRegistries");
 
-    TILES = new TypeRegistry<Tile>(new ResourceLocation(VoltsOfDoom.getId(), "tiles"),
-        RegistryTypes.TILES);
-    ENTITIES = new TypeRegistry<Entity>(new ResourceLocation(VoltsOfDoom.getId(), "entities"),
-        RegistryTypes.ENTITIES);
-    ADVENTURES = new TypeRegistry<Adventure>(
-        new ResourceLocation(VoltsOfDoom.getId(), "adventures"), RegistryTypes.ADVENTURES);
+    TILES = new TypeRegistry<Tile>(new ResourceLocation(VoltsOfDoom.getId(), "tiles"), RegistryTypes.TILES);
+    ENTITIES = new TypeRegistry<Entity>(new ResourceLocation(VoltsOfDoom.getId(), "entities"), RegistryTypes.ENTITIES);
+    ADVENTURES = new TypeRegistry<Adventure>(new ResourceLocation(VoltsOfDoom.getId(), "adventures"), RegistryTypes.ADVENTURES);
 
     event.submit(TILES);
     event.submit(ENTITIES);
